7c2b9b924a8adf5ce515c223cb711f34283d60f8
Paul K <paulmkouadio@gmail.com>
2023-11-20 15:14:10 +0000

    feat(tutorial): add pages to complete step 2

--- src/app/repos/page.js
+++ src/app/repos/page.js
@@ -1,7 +1,24 @@
-`use client`
+'use client';
+import { Grid, Column } from '@carbon/react';
+import RepoTable from './RepoTable';
+
+const headers = [
+  {
+    key: 'name',
+    header: 'Name',
+  },
+  {
+    key: 'createdAt',
+    header: 'Created',
+  },
+];
 
 export default function RepoPage() {
-    return (
-        <div>REPO PAGE</div>
-    );
-};
+  return (
+    <Grid className="repo-page">
+      <Column lg={16} md={8} sm={4} className="repo-page__r1">
+        <RepoTable headers={headers} rows={rows} />
+      </Column>
+    </Grid>
+  );
+}
